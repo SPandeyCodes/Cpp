6b24822c3c68cd2b5dc1450033b4d64af4add030
Sailesh Pandey <69977764+SPandeyCodes@users.noreply.github.com>
2020-09-14 23:44:14 +0530

    Data Types

---
+++ datatypess.cpp
@@ -0,0 +1,38 @@
+/* DATA TYPES */
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+   int num1= 007;
+
+   int num2=-9;
+ char character ='A';
+ string name="sailesh pandey";
+
+
+  //double and float are both used for floating numbers but use double
+  
+  double num3=2.2;
+  
+  bool isMale=true;
+  bool isFemale= false;
+  
+  cout<<"the interger value 1 is" << num1 << endl;
+  cout << "the integer value 2 is " << num2 << endl;
+  cout << "the character is "<< character<< endl;
+  
+  cout<< "the string is "<< name<< endl;
+   cout<< " the floating number is" << num3<<endl;
+   cout<< " the bool is " << isMale<<endl;
+    
+      
+
+return 0;
+
+
+
+}
