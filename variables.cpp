0e8a55345cc36c13edbe00a91b4123258bb8e715
Sailesh Pandey <69977764+SPandeyCodes@users.noreply.github.com>
2020-09-14 23:42:14 +0530

    Variables

---
+++ variables.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int main()
+
+{
+
+int age=55;  // age is a variable
+string name= "Rocky";  //name is a variable
+
+cout<< "the name is "<< name<<endl;
+cout << "the age is "<< age<<endl;
+
+
+return 0;
+}
+
+// so if i want to change the name and age in a whole paragraph in about 500 lines ,
+// i can do it easily by changing it in the definition of variables and that will cahnge it all across the paragraph
