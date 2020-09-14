a13eafa0587c11ebf1234e84dbfc62030ea7554f
Sailesh Pandey <69977764+SPandeyCodes@users.noreply.github.com>
2020-09-14 23:47:58 +0530

    mad libs

---
+++ madlibs.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+
+
+ using namespace std;
+  int main()
+  
+  {
+  	  string color, pluralNoun, celebrity;
+  	  
+  	  cout<<"enter the color"<<endl;
+  	  getline(cin,color);  // ask input from users and store in color
+  	  cout<<"enter the pluralNoun"<<endl;
+  	  getline(cin,pluralNoun);
+  	  cout<<"enter the celebrity"<<endl;
+  	  getline(cin,celebrity);
+  	
+  	    
+  	  cout<<"Roses are "<<color <<endl;
+  	  cout<<pluralNoun<< " are blue"<<endl;
+  	  cout<< "I love" <<celebrity<<endl;
+  	
+  }
