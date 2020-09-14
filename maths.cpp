b4723af22d8be849a6b0f9390e6c2e8de1717f42
Sailesh Pandey <69977764+SPandeyCodes@users.noreply.github.com>
2020-09-14 23:45:33 +0530

    maths

---
+++ maths.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+	 int num1, num2;
+	 int sum=0;
+	 
+	  cout<< "Enter the first number "<<endl;
+	  cin>>num1;
+	  
+	  cout<< "Enter the second number "<<endl;
+	  cin>>num2;
+	  
+	  sum=num1+num2;
+	  
+	  cout<<sum<<endl;
+	 
+	
+	
+	
+	return 0;
+}
