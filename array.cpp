5fad2e2f6ae1c8ef260909463233fbc8f04f2651
Sailesh Pandey <69977764+SPandeyCodes@users.noreply.github.com>
2020-09-15 22:49:21 +0530

    array

---
+++ array.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+
+
+using namespace std;
+
+int  main()
+
+{
+	 int nums[]={1,2,3,4,5,6};
+	 
+	 cout<< nums[2]<<endl;
+	 nums[0]=20;
+	 cout<<nums[0]<<endl;
+	 
+	 nums[6]=55;
+	 cout<<nums[6]<<endl;
+	 
+	 
+	
+	
+	return 0;
+	
+	
+}
